8504f818425e296ab07f8647c75bdd1b9f846fe4
unknown <thakaliishneha_31@icloud.com>
2024-06-27 22:06:45 +0545

    some changes made

--- coverpage.py
+++ coverpage.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from PIL import Image,ImageTk
 cover=Tk()
-cover.title("page")
+cover.title("Main Page")
 cover.minsize(width=800,height=900)
 cover.maxsize(width=1000,height=1300)
 
@@ -13,57 +13,45 @@ final_image1=ImageTk.PhotoImage(resize_photo1)
 medical_team=Label(cover,image=final_image1,width=0,height=300)
 medical_team.place(x=0,y=20)
 
-# def login_page():
-#     window = Tk()
-#     window.title("Simon Hospital")
-#     window.configure(bg='sky blue') # background color 
-#     window.minsize(width=1200,height=500)
+
 def Login_page_2():
     window = Tk()
-    window.configure(bg='sky blue') # background color
-    l2 = Label(window,text="Medical Team",bg = 'black',fg='white',font=20)
+    window.title("Medical Team login")
+    window.configure(bg='#AFEEEE') # background color
+    l2 = Label(window,text="Medical Team",bg="#AFEEEE",fg='black',font=20)
     l2.place(x = 10,y=20)
-    l3 = Label(window,text='USER NAME :-',bg='green',fg='white')
+    l3 = Label(window,text='USERNAME:-',bg='#AFEEEE',fg='black')
     l3.place(x=10,y=60)
     e1 = Entry(window,width=20,font=("Calibri",10))
     e1.place(x =100,y=59)
-    l4 = Label(window,text='PASSWORD :-',bg= 'green',fg ='white')
+    l4 = Label(window,text='PASSWORD:-',bg= '#AFEEEE',fg ='black')
     l4.place(x =10,y=100)
     e2 = Entry(window,width=20,font=("Calibri",10))
     e2.place(x =100,y=100)
-    btn = Button(window,text='LOGIN',bg='red')
+    btn = Button(window,text='LOGIN',bg='white')
     btn.place(x=95,y=140)
     
 def reg_p():
-    window = Tk()
-    l=Label(window,text="Fill all the details",font = 100)
-    l.place(x= 250)
-    l1 = Label(window,text="First Name",font=50)
-    l1.place(x=0,y=50)
-    e1= Entry(window,width=20,font=("Calibri",10))
-    e1.place(x=100,y=54)
-    l2 = Label(window,text="Last Name",font = 50)
-    l2.place(x=0,y=100)
-    e2 = Entry(window,width=20,font=("Calibri",10))
-    e2.place(x =100,y=105)
+    import reg
     
 def login_page():
     window = Tk()
-    window.configure(bg='sky blue') # background color
-    l2 = Label(window,text="For Patient",bg = 'black',fg='white',font=20)
+    window.title('Patient login')
+    window.configure(bg='#AFEEEE') # background color
+    l2 = Label(window,text="For Patient",bg = '#AFEEEE',fg='black',font=20)
     l2.place(x = 10,y=20)
-    l3 = Label(window,text='USER NAME :-',bg='green',fg='white')
+    l3 = Label(window,text='USER ID:-',bg='#AFEEEE',fg='black')
     l3.place(x=10,y=60)
     e1 = Entry(window,width=20,font=("Calibri",10))
     e1.place(x =100,y=59)
-    l4 = Label(window,text='PASSWORD :-',bg= 'green',fg ='white')
+    l4 = Label(window,text='PASSWORD :-',bg= '#AFEEEE',fg ='black')
     l4.place(x =10,y=100)
     e2 = Entry(window,width=20,font=("Calibri",10))
     e2.place(x =100,y=100)
-    btn = Button(window,text='LOGIN',bg='red')
-    btn.place(x=95,y=140)
-    btn1 = Button(window,text="Create New account",bg='Green',command = reg_p)
-    btn1.place(x=95,y=180)
+    btn = Button(window,text='LOGIN',bg='white')
+    btn.place(x=150,y=140)
+    btn1 = Button(window,text="Create New account",bg='white',command = reg_p)
+    btn1.place(x=0,y=140)
     
 staff=Button(cover,text="For medical team",font=800,command=Login_page_2)
 staff.place(x=20,y=280)
@@ -79,6 +67,4 @@ patient_team.place(x=500,y=20)
 patient=Button(cover,text="For patient",font=800,command=login_page)
 patient.place(x=550,y=280)
 
-
-if __name__ == "__main__":
-    mainloop()
+mainloop()
--- reg.py
+++ reg.py
@@ -1,3 +1,28 @@
 from tkinter import *
 from PIL import Image,ImageTk
-import coverpage
+window = Tk()
+window.title('Patient Registration')
+l=Label(window,text="Fill all the details",font = 100)
+l.place(x= 250)
+l1 = Label(window,text="First Name",font=50)
+l1.place(x=0,y=50)
+e1= Entry(window,width=20,font=("Calibri",10))
+e1.place(x=100,y=54)
+l2 = Label(window,text="Last Name",font = 50)
+l2.place(x=0,y=100)
+e2 = Entry(window,width=20,font=("Calibri",10))
+e2.place(x =100,y=105)
+
+g=Label(text="Choose your gender:",font=50)
+g.place(x=0,y=150)
+var=IntVar()
+r1=Radiobutton(window,text="Male",variable=var,value=1)
+r1.place()
+r2=Radiobutton(window,text="Female",variable=var,value=2)
+r2.place()
+r3=Radiobutton(window,text="Others",variable=var,value=3)
+r3.place()
+
+mainloop()
+
+
