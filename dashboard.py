eedf231fd3b14f357d28d1ca4f725ee6116abbd0
unknown <thakaliishneha_31@icloud.com>
2024-07-14 21:04:30 +0545

    changes

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,7 @@ root.title("Hope Hospital")
 root.iconbitmap("icon.ico")
 root.minsize(height=900,width=700)
 root.maxsize(height=900,width=700)
-root.resizable(0,0)
+#root.resizable(0,0)
 hospital=Label(root,text="Dashboard",font=("Arial Bold",10))
 hospital.pack()
 
