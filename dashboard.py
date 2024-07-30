820dfe39691fd390001b1cf28b75814e5b33758f
unknown <thakaliishneha_31@icloud.com>
2024-07-30 10:13:51 +0545

    homepage added

--- dashboard.py
+++ dashboard.py
@@ -6,11 +6,11 @@ root.iconbitmap("icon.ico")
 root.minsize(height=900,width=700)
 root.maxsize(height=900,width=700)
 #root.resizable(0,0)
-hospital=Label(root,text="Dashboard",font=("Arial Bold",10))
+hospital=Label(root,text="Hope Hospital",font=("Arial Bold",10))
 hospital.pack()
 
 # Doctor Lists
-doctors=Label(root,text="Doctors List",font=("Arial Bold",30))
+doctors=Label(root,text="Our Doctors",font=("Arial Bold",30))
 doctors.pack(padx=0,pady=0,anchor="center")
 
 
@@ -120,7 +120,7 @@ image5=Image.open('./assets/image/doctor5.png')
 img_size=image5.resize((150,150))
 image5 = ImageTk.PhotoImage(img_size)
 
-#create a label to display the image
+#create a label to display the image/
 image_label = Label(frame5, image=image5, justify="center")
 image_label.pack(padx=0, pady=0, anchor="e")
 doc_name=Label(frame5,text="David Kaif",font=("Arial",14), justify="right")
--- homepage.py
+++ homepage.py
@@ -6,12 +6,35 @@ root.iconbitmap('icon.ico')
 root.minsize(height=900,width=700)
 root.maxsize(height=900,width=700)
 
-background=Image.open("homepage1.png")
-back_ground1=background.resize((1000,700))
-background1=ImageTk.PhotoImage(back_ground1)
-back_ground=Label(root,image=background1)
-back_ground.place(x=0,y=0)
 
+#for importing info
+def info():
+    root.destroy()
+    import about_us
+
+
+#for information icon
+info=Label(root,text="About us",font=("Helvetica", 20))
+info.place(x=0,y=120)
+info_pic=Image.open("home.ico")
+resize_info_pic=info_pic.resize((40,60))
+final_info_image=ImageTk.PhotoImage(resize_info_pic)
+info_icon=Button(root, image=final_info_image, width=0, height=0,command=info)
+info_icon.place(x=35,y=50)
+
+#import dashboard
+def doc():
+    root.destroy()
+    import dashboard
+
+#for doctors
+doctor=Label(root,text="Our Doctors",font=("Helvetica", 20))
+doctor.place(x=250,y=120)
+doctor_pic=Image.open("doc.ico")
+resize_doc_pic=doctor_pic.resize((40,60))
+final_doc_image=ImageTk.PhotoImage(resize_doc_pic)
+doc_icon=Button(root,image=final_doc_image,width=0,height=0,command=doc)
+doc_icon.place(x=300,y=50)
 
 
 
